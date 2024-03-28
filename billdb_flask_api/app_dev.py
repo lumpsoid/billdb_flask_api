6abec05492866a87f2ad0969efb3941c20f16751
lumpsoid <mmgeeki@gmail.com>
2024-03-28 14:54:40 +0100

    add dev app file for local testing

---
+++ billdb_flask_api/app_dev.py
@@ -0,0 +1,34 @@
+try:
+    from flask import Flask
+except ImportError:
+    raise ImportError('flask packed was not found. Try `pip install .[api]`')
+from flutter_app_routs import flutter_app
+from web_routs import browser_app
+
+import logging
+
+def create_app():
+    app = Flask(__name__)
+    # use appropriate path for the database
+    app.config['DATABASE_PATH'] = None
+    app.register_blueprint(flutter_app)
+    app.register_blueprint(browser_app)
+    return app
+    
+
+# Configure Flask logging
+# app.logger.setLevel(logging.DEBUG)  # Set the desired logging level
+# # handler = logging.FileHandler("./flask.log")  # Replace with the desired path inside the container
+# handler = logging.StreamHandler()
+# logging.basicConfig(
+#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+# )
+
+# handler.setLevel(level=logging.DEBUG)  # Set the desired logging level for Flask logs
+# app.logger.addHandler(handler)
+
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
